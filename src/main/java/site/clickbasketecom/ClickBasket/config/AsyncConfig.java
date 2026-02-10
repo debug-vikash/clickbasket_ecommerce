05dbc3358ed38606e9adbfa72fc308adc0469ecc
debug-vikash <vikashmaurya861@gmail.com>
2026-02-10 14:20:42 +0530

    Payment updated

---
+++ src/main/java/site/clickbasketecom/ClickBasket/config/AsyncConfig.java
@@ -0,0 +1,12 @@
+package site.clickbasketecom.ClickBasket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+/**
+ * Configuration for async operations.
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
